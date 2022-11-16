7d3387b536ac1c1784e4e89839ff9796cc3f8721
Victor Tavares <victortavaresdev@gmail.com>
2022-11-16 17:59:25 -0300

    chat app

---
+++ server/src/chat/chat.gateway.ts
@@ -0,0 +1,18 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway({ cors: { origin: '*' } })
+export class ChatGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string): any {
+    this.server.emit('message', message);
+  }
+}
